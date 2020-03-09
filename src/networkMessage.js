d814c897546f55ca5b654a577abe224e9e3e5a1b
Mateusz Hołowieszko <holowieszko.m@gmail.com>
2020-03-09 18:13:08 +0100

    Added method thats reset buffer offset to the default value

--- src/networkMessage.js
+++ src/networkMessage.js
@@ -1,3 +1,4 @@
+// src/networkMessage.js
 class NetworkMessage {
   constructor() {
     this.buffer = [];
@@ -8,6 +9,10 @@ class NetworkMessage {
     const v = (this.buffer[this.offset] | (this.buffer[this.offset + 1] << 8))
     this.offset += 2;
     return v;
+  }
+
+  reset() {
+    this.offset = 0;
   }
 }
 
